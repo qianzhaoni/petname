71978a00583685f0126b59d8e6d8e7ddb35201cb
qianzhaoni <qianzhaoni@gmail.com>
2024-11-13 07:56:01 +0800

    23

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist',
+    outDir: './dist',
     emptyOutDir: true,
     rollupOptions: {
       output: {
